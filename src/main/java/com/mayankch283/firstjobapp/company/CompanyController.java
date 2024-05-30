6a9fa0afff671b7308f34af18c149a53df9deddc
mayankch283 <mayankch283@gmail.com>
2024-05-30 10:02:52 +0530

    <feat> added create, update, find and delete company features

--- src/main/java/com/mayankch283/firstjobapp/company/CompanyController.java
+++ src/main/java/com/mayankch283/firstjobapp/company/CompanyController.java
@@ -1,9 +1,9 @@
 package com.mayankch283.firstjobapp.company;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.RequestEntity;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -17,8 +17,40 @@ public class CompanyController {
     }
 
     @GetMapping
-    public List<Company> getAllCompanies() {
-        return companyService.getAllCompanies();
+    public ResponseEntity<List<Company>> getAllCompanies() {
+        return ResponseEntity.ok(companyService.getAllCompanies());
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity<Company> findCompany(@PathVariable long id) {
+        Company company = companyService.findCompany(id);
+        if (company!=null) {
+            return new ResponseEntity<>(company, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<String> updateCompany(@RequestBody Company company, @PathVariable Long id) {
+        if (companyService.updateCompany(company, id)) {
+            return new ResponseEntity("Company updated successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity("No company found", HttpStatus.NOT_FOUND);
+    }
+
+    @PostMapping
+    public ResponseEntity<String> createCompany(@RequestBody Company company) {
+        companyService.createCompany(company);
+        return new ResponseEntity("Company created successfully", HttpStatus.CREATED);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteCompany(@PathVariable long id) {
+        Company companyToBeDeleted = companyService.findCompany(id);
+        if(companyToBeDeleted != null){
+            companyService.deleteCompany(companyToBeDeleted);
+            return new ResponseEntity("Company deleted successfully", HttpStatus.OK);
+        }
+        return new ResponseEntity("Company not found", HttpStatus.NOT_FOUND);
+    }
 }
--- src/main/java/com/mayankch283/firstjobapp/company/CompanyServiceImpl.java
+++ src/main/java/com/mayankch283/firstjobapp/company/CompanyServiceImpl.java
@@ -3,6 +3,7 @@ package com.mayankch283.firstjobapp.company;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CompanyServiceImpl implements CompanyService{
@@ -16,5 +17,34 @@ public class CompanyServiceImpl implements CompanyService{
     @Override
     public List<Company> getAllCompanies() {
         return  companyRepository.findAll();
+    }
+
+    @Override
+    public boolean updateCompany(Company company, Long id){
+        Optional<Company> companyOptional = companyRepository.findById(id);
+        if(companyOptional.isPresent()){
+            Company companyToUpdate = companyOptional.get();
+            companyToUpdate.setName(company.getName());
+            companyToUpdate.setDescription(company.getDescription());
+            companyToUpdate.setJobs(company.getJobs());
+            companyRepository.save(companyToUpdate);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public void createCompany(Company company) {
+        companyRepository.save(company);
+    }
+
+    @Override
+    public Company findCompany(long id) {
+        return companyRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void deleteCompany(Company company) {
+        companyRepository.delete(company);
     }
 }
